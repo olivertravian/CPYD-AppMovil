a94cd33e8a0e9ca4dafd60c97902839c836326ca
Enrique Villalobos <enrique.lnx@gmail.com>
2022-12-04 00:49:27 -0300

    Uploading the others screens as templates

---
+++ lib/screen/polls.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PollsScreen extends StatelessWidget {
+  const PollsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff00417c),
+      padding: const EdgeInsets.symmetric(vertical: 20, ),
+      child: Column(),
+    );
+  }
+}
---
+++ lib/screen/vote.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VoteScreen extends StatelessWidget {
+  const VoteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff00417c),
+      padding: const EdgeInsets.symmetric(vertical: 20, ),
+      child: Column(),
+    );
+  }
+}
